650df689557a7882967b402e965058d6633bab09
Lee Curtis <leec313@gmail.com>
2023-06-22 15:19:14 +0000

    added skeleton of functions and docstrings

--- run.py
+++ run.py
@@ -12,8 +12,43 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lc_bank')
 
-test = SHEET.worksheet('current')
 
-test_data = test.get_all_values()
 
-print(test_data)
+def login():
+    """
+    Prompts user to enter their name & password
+    If name and password match, the user can access the next page
+    And the next function will run
+    Also acts as a logout function as when it is called, it will 
+    return the user to the login screen
+    """
+
+def options():
+    """
+    Asks user to select options on what they would like to do next
+    Options may include withdraw, deposit, check balance etc.
+    """
+
+def withdraw():
+    """
+    Allows user to withdraw from the account of their choosing
+    Minus input amount from the current balance
+    """
+
+def deposit():
+    """
+    Allows user to deposit from the account of their choosing
+    Adds input amount from the current balance
+    """
+
+def check():
+    """
+    Allows user to check the balance in the account of their choosing
+    """
+
+def main():
+    """
+    Initialises all program functions
+    """
+
+main()
